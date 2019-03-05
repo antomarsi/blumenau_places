9a9678ec686718c1932654c556d6b656b8bde920
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-03-05 10:08:39 -0300

    Updates

---
+++ precache-manifest.72220bb82cfcd6e5f57f0b3d90264429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "babdc39fa6a5561be677",
+    "url": "/static/js/main.babdc39f.chunk.js"
+  },
+  {
+    "revision": "e555ba9f8f67bf864774",
+    "url": "/static/js/2.e555ba9f.chunk.js"
+  },
+  {
+    "revision": "babdc39fa6a5561be677",
+    "url": "/static/css/main.3591de5f.chunk.css"
+  },
+  {
+    "revision": "e555ba9f8f67bf864774",
+    "url": "/static/css/2.3e5b92c0.chunk.css"
+  },
+  {
+    "revision": "ed8fabccc8a28ee1e463e3144b1b17f8",
+    "url": "/index.html"
+  }
+];
