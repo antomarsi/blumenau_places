5caf3a4d76cab03e9f671da6d0d3533c330fc0df
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-03-06 14:24:07 -0300

    Removed Hover state

--- src/Components/NewMarker.js
+++ src/Components/NewMarker.js
@@ -4,26 +4,13 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import "./NewMarker.css";
 
 class NewMarker extends Component {
-  state = {
-    hover: false
-  };
-
-  onMouseEnterHandler = () => {
-    this.setState({ hover: true });
-  };
-  onMouseExitHandler = () => {
-    this.setState({ hover: false });
-  };
 
   render = () => {
     const { text, selected, selectPlaceHandle } = this.props;
-    const { hover } = this.state;
     return (
       <div
         title={text}
         className={selected ? "marker selected" : "marker"}
-        onMouseEnter={this.onMouseEnterHandler}
-        onMouseLeave={this.onMouseExitHandler}
         onClick={() => selectPlaceHandle()}
         >
             <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
