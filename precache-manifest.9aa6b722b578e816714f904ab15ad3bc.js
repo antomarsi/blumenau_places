76a926be3318fda9b39c90907e32bcd584d1048a
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-03-06 14:30:51 -0300

    Updates

---
+++ precache-manifest.9aa6b722b578e816714f904ab15ad3bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d5582081c7c823b9c3",
+    "url": "/blumenau_places/static/js/runtime~main.46d55820.js"
+  },
+  {
+    "revision": "c357109083ac9aa65390",
+    "url": "/blumenau_places/static/js/main.c3571090.chunk.js"
+  },
+  {
+    "revision": "f1f12119ede05d07804a",
+    "url": "/blumenau_places/static/js/2.f1f12119.chunk.js"
+  },
+  {
+    "revision": "c357109083ac9aa65390",
+    "url": "/blumenau_places/static/css/main.1caf6c25.chunk.css"
+  },
+  {
+    "revision": "f1f12119ede05d07804a",
+    "url": "/blumenau_places/static/css/2.3e5b92c0.chunk.css"
+  },
+  {
+    "revision": "47690ec06cfe179dc2a8000184b050dd",
+    "url": "/blumenau_places/index.html"
+  }
+];
