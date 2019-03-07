b2263c9ebf415633abeb17e7afbee8837c26e631
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-03-07 11:58:46 -0300

    Alterado classe para componente funcional

--- src/Components/NewMarker.js
+++ src/Components/NewMarker.js
@@ -1,22 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import "./NewMarker.css";
 
-class NewMarker extends Component {
-
-  render = () => {
-    const { text, selected, selectPlaceHandle } = this.props;
-    return (
-      <div
-        title={text}
-        className={selected ? "marker selected" : "marker"}
-        onClick={() => selectPlaceHandle()}
-        >
-            <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
-      </div>
-    );
-  }
-}
+const NewMarker = ({ text, selected, selectPlaceHandle }) => {
+  return (
+    <div
+      title={text}
+      className={selected ? "marker selected" : "marker"}
+      onClick={() => selectPlaceHandle()}
+    >
+      <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
+    </div>
+  );
+};
 
 export default NewMarker;
