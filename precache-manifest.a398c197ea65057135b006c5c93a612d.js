6ecb8067d48480c17cef678c8cd030f6b1efc0f7
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-03-07 12:00:47 -0300

    Updates

--- precache-manifest.a398c197ea65057135b006c5c93a612d.js
+++ precache-manifest.a398c197ea65057135b006c5c93a612d.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/blumenau_places/static/js/runtime~main.46d55820.js"
   },
   {
-    "revision": "17182d93ad12bdb0b72e",
-    "url": "/blumenau_places/static/js/main.17182d93.chunk.js"
+    "revision": "83b15cf2f97ea082d304",
+    "url": "/blumenau_places/static/js/main.83b15cf2.chunk.js"
   },
   {
-    "revision": "f1f12119ede05d07804a",
-    "url": "/blumenau_places/static/js/2.f1f12119.chunk.js"
+    "revision": "9db4dea6c6ee23db2905",
+    "url": "/blumenau_places/static/js/2.9db4dea6.chunk.js"
   },
   {
-    "revision": "17182d93ad12bdb0b72e",
+    "revision": "83b15cf2f97ea082d304",
     "url": "/blumenau_places/static/css/main.2d201479.chunk.css"
   },
   {
-    "revision": "f1f12119ede05d07804a",
+    "revision": "9db4dea6c6ee23db2905",
     "url": "/blumenau_places/static/css/2.3e5b92c0.chunk.css"
   },
   {
-    "revision": "5f664efad53b03aee2c148076e471a7d",
+    "revision": "77d3938ad302a1f2ceb4dd26cb70c2d4",
     "url": "/blumenau_places/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/blumenau_places/precache-manifest.30a65ca874e03182b24d5e59c5d046dd.js"
+  "/blumenau_places/precache-manifest.a398c197ea65057135b006c5c93a612d.js"
 );
 
 workbox.clientsClaim();
